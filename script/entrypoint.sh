11853e6bb29ff5d99f8fc4673a88cc0b6aa7fa07
neilharia7 <neil.haria@thinkanalytics.in>
2020-07-25 01:28:16 +0530

    updated entrypoint.sh and airflow config

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,147 +1,78 @@
 #!/usr/bin/env bash
 
-# User-provided configuration must always be respected.
-#
-# Therefore, this script must only derives Airflow AIRFLOW__ variables from other variables
-# when the user did not provide their own configuration.
+CMD="airflow"
+TRY_LOOP="${TRY_LOOP:-10}"
+MYSQL_HOST="${MYSQL_HOST:-testserver.crhcifgoezvo.ap-south-1.rds.amazonaws.com}"
+MYSQL_PORT=3306
+MYSQL_CREDS="${MYSQL_CREDS:-thinktest:testadmin}"
+MYSQL_USER="${MYSQL_USER:-thinktest}"
+MYSQL_PASSWORD="${MYSQL_PASSWORD:-testadmin}"
+MYSQL_DB="${MYSQL_DB:-airflow}"
+RABBITMQ_HOST="${RABBITMQ_HOST:-rabbitmq}"
+RABBITMQ_CREDS="${RABBITMQ_CREDS:-airflow:airflow}"
+RABBITMQ_MANAGEMENT_PORT=15672
+FLOWER_URL_PREFIX="${FLOWER_URL_PREFIX:-}"
+AIRFLOW_URL_PREFIX="${AIRFLOW_URL_PREFIX:-}"
+LOAD_DAGS_EXAMPLES="${LOAD_DAGS_EXAMPLES:-false}"
 
-TRY_LOOP="20"
-
-# Global defaults and back-compat
-: "${AIRFLOW_HOME:="/usr/local/airflow"}"
-: "${AIRFLOW__CORE__FERNET_KEY:=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}}"
-: "${AIRFLOW__CORE__EXECUTOR:=${EXECUTOR:-Sequential}Executor}"
-
-# Load DAGs examples (default: Yes)
-if [[ -z "$AIRFLOW__CORE__LOAD_EXAMPLES" && "${LOAD_EX:=n}" == n ]]; then
-  AIRFLOW__CORE__LOAD_EXAMPLES=False
+if [[ -z ${FERNET_KEY} ]]; then
+	FERNET_KEY=$(python3 -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")
 fi
 
-export \
-  AIRFLOW_HOME \
-  AIRFLOW__CORE__EXECUTOR \
-  AIRFLOW__CORE__FERNET_KEY \
-  AIRFLOW__CORE__LOAD_EXAMPLES \
+echo "MYSQL HOST: " ${MYSQL_HOST}
+echo "RABBITMQ HOST: " ${RABBITMQ_HOST}
+echo "LOAD DAG EXAMPLES: " ${LOAD_DAGS_EXAMPLES}
+echo "AIRFLOW HOME: " ${AIRFLOW_HOME}
 
-# Install custom python package if requirements.txt is present
-if [ -e "/requirements.txt" ]; then
-    $(command -v pip) install --user -r /requirements.txt
-fi
+# Generate Fernet key
+sed -i "s/{{ FERNET_KEY }}/${FERNET_KEY}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s/{{ MYSQL_HOST }}/${MYSQL_HOST}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s/{{ MYSQL_CREDS }}/${MYSQL_CREDS}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s/{{ MYSQL_DB }}/${MYSQL_DB}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s/{{ RABBITMQ_HOST }}/${RABBITMQ_HOST}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s/{{ RABBITMQ_CREDS }}/${RABBITMQ_CREDS}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s/{{ LOAD_DAGS_EXAMPLES }}/${LOAD_DAGS_EXAMPLES}/" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s#{{ FLOWER_URL_PREFIX }}#${FLOWER_URL_PREFIX}#" ${AIRFLOW_HOME}/airflow.cfg
+sed -i "s#{{ AIRFLOW_URL_PREFIX }}#${AIRFLOW_URL_PREFIX}#" ${AIRFLOW_HOME}/airflow.cfg
 
-wait_for_port() {
-  local name="$1" host="$2" port="$3"
-  local j=0
-  while ! nc -z "$host" "$port" >/dev/null 2>&1 < /dev/null; do
-    j=$((j+1))
-    if [ $j -ge $TRY_LOOP ]; then
-      echo >&2 "$(date) - $host:$port still not reachable, giving up"
+AIRFLOW__CORE__SQL_ALCHEMY_CONN="mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}"
+# AIRFLOW__CELERY__RESULT_BACKEND="db+mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}"
+
+echo "SQL: " ${AIRFLOW__CORE__SQL_ALCHEMY_CONN}
+
+export AIRFLOW__CORE__SQL_ALCHEMY_CONN AIRFLOW__CELERY__RESULT_BACKEND
+
+# wait for rabbitmq
+if [[ "$1" = "webserver" ]] || [[ "$1" = "worker" ]] || [[ "$1" = "scheduler" ]] || [[ "$1" = "flower" ]] ; then
+  j=0
+  while ! curl -sI -u ${RABBITMQ_CREDS} http://${RABBITMQ_HOST}:${RABBITMQ_MANAGEMENT_PORT}/api/whoami |grep '200 OK'; do
+    j=`expr ${j} + 1`
+    if [[ ${j} -ge ${TRY_LOOP} ]]; then
+      echo "$(date) - $RABBITMQ_HOST still not reachable, giving up"
       exit 1
     fi
-    echo "$(date) - waiting for $name... $j/$TRY_LOOP"
+    echo "$(date) - waiting for RabbitMQ... $j/$TRY_LOOP"
     sleep 5
   done
-}
+fi
 
-# Other executors than SequentialExecutor drive the need for an SQL database, here PostgreSQL is used
-if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
-  # Check if the user has provided explicit Airflow configuration concerning the database
-  if [ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]; then
-
-    : "${MYSQL_HOST:="testserver.crhcifgoezvo.ap-south-1.rds.amazonaws.com"}"
-    : "${MYSQL_PORT:="3306"}"
-    : "${MYSQL_USER:="thinktest"}"
-    : "${MYSQL_PASSWORD:="testadmin"}"
-    : "${MYSQL_DB:="airflow"}"
-    # Default values corresponding to the default compose files
-    #    : "${POSTGRES_HOST:="postgres"}"
-    #    : "${POSTGRES_PORT:="5432"}"
-    #    : "${POSTGRES_USER:="airflow"}"
-    #    : "${POSTGRES_PASSWORD:="airflow"}"
-    #    : "${POSTGRES_DB:="airflow"}"
-    #    : "${POSTGRES_EXTRAS:-""}"
-
-    #    AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
-    AIRFLOW__CORE__SQL_ALCHEMY_CONN="mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}"
-    export AIRFLOW__CORE__SQL_ALCHEMY_CONN
-
-    # Check if the user has provided explicit Airflow configuration for the broker's connection to the database
-    if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
-      #      AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
-      AIRFLOW__CELERY__RESULT_BACKEND="db+mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB}"
-      export AIRFLOW__CELERY__RESULT_BACKEND
-    fi
-  else
-    if [[ "$AIRFLOW__CORE__EXECUTOR" == "CeleryExecutor" && -z "$AIRFLOW__CELERY__RESULT_BACKEND" ]]; then
-      >&2 printf '%s\n' "FATAL: if you set AIRFLOW__CORE__SQL_ALCHEMY_CONN manually with CeleryExecutor you must also set AIRFLOW__CELERY__RESULT_BACKEND"
+# wait for postgres
+if [[ "$1" = "webserver" ]] || [[ "$1" = "worker" ]] || [[ "$1" = "scheduler" ]] ; then
+  i=0
+  while ! nc ${MYSQL_HOST} ${MYSQL_PORT} >/dev/null 2>&1 < /dev/null; do
+    i=`expr ${i} + 1`
+    if [[ ${i} -ge ${TRY_LOOP} ]]; then
+      echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up"
       exit 1
     fi
-
-    # Derive useful variables from the AIRFLOW__ variables provided explicitly by the user
-    #    POSTGRES_ENDPOINT=$(echo -n "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" | cut -d '/' -f3 | sed -e 's,.*@,,')
-    #    POSTGRES_HOST=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f1)
-    #    POSTGRES_PORT=$(echo -n "$POSTGRES_ENDPOINT" | cut -d ':' -f2)
-    MYSQL_ENDPOINT=$(echo -n "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" | cut -d '/' -f3 | sed -e 's,.*@,,')
-    MYSQL_HOST=$(echo -n "$MYSQL_ENDPOINT" | cut -d ':' -f1)
-    MYSQL_PORT=$(echo -n "$MYSQL_ENDPOINT" | cut -d ':' -f2)
+    echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}... $i/$TRY_LOOP"
+    sleep 5
+  done
+  if [[ "$1" = "webserver" ]]; then
+    echo "Initialize database..."
+    echo "$CMD initdb"
+    ${CMD} initdb
   fi
-
-    #  wait_for_port "Postgres" "$POSTGRES_HOST" "$POSTGRES_PORT"
-  wait_for_port "Postgres" "$MYSQL_HOST" "$MYSQL_PORT"
 fi
 
-# CeleryExecutor drives the need for a Celery broker, here Redis is used
-if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
-  # Check if the user has provided explicit Airflow configuration concerning the broker
-  if [ -z "$AIRFLOW__CELERY__BROKER_URL" ]; then
-    # Default values corresponding to the default compose files
-    : "${REDIS_PROTO:="redis://"}"
-    : "${REDIS_HOST:="redis"}"
-    : "${REDIS_PORT:="6379"}"
-    : "${REDIS_PASSWORD:=""}"
-    : "${REDIS_DBNUM:="1"}"
-
-    # When Redis is secured by basic auth, it does not handle the username part of basic auth, only a token
-    if [ -n "$REDIS_PASSWORD" ]; then
-      REDIS_PREFIX=":${REDIS_PASSWORD}@"
-    else
-      REDIS_PREFIX=
-    fi
-
-    AIRFLOW__CELERY__BROKER_URL="${REDIS_PROTO}${REDIS_PREFIX}${REDIS_HOST}:${REDIS_PORT}/${REDIS_DBNUM}"
-    export AIRFLOW__CELERY__BROKER_URL
-  else
-    # Derive useful variables from the AIRFLOW__ variables provided explicitly by the user
-    REDIS_ENDPOINT=$(echo -n "$AIRFLOW__CELERY__BROKER_URL" | cut -d '/' -f3 | sed -e 's,.*@,,')
-    REDIS_HOST=$(echo -n "$MYSQL_ENDPOINT" | cut -d ':' -f1)
-    REDIS_PORT=$(echo -n "$MYSQL_ENDPOINT" | cut -d ':' -f2)
-  fi
-
-  wait_for_port "Redis" "$REDIS_HOST" "$REDIS_PORT"
-fi
-
-case "$1" in
-  webserver)
-    airflow initdb
-    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ] || [ "$AIRFLOW__CORE__EXECUTOR" = "SequentialExecutor" ]; then
-      # With the "Local" and "Sequential" executors it should all run in one container.
-      airflow scheduler &
-    fi
-    exec airflow webserver
-    ;;
-  worker|scheduler)
-    # Give the webserver time to run initdb.
-    sleep 10
-    exec airflow "$@"
-    ;;
-  flower)
-    sleep 10
-    exec airflow "$@"
-    ;;
-  version)
-    exec airflow "$@"
-    ;;
-  *)
-    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
-    exec "$@"
-    ;;
-esac
+${CMD} "$@"
