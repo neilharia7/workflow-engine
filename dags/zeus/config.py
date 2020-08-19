ff6a707b900df9219fb4e974797f9eafc6c7d718
neilharia7 <neil.haria@thinkanalytics.in>
2020-08-19 11:41:49 +0530

    bug fix, mistakenly deleted file reverted.

--- dags/zeus/config.py
+++ dags/zeus/config.py
@@ -6,3 +6,8 @@ class Config:
 	class Triggers:
 		one_success = "one_success"
 		all_done = "all_done"
+		
+	class AWS:
+		class S3:
+			bucket_name = "flowxpert"
+			key_path = "airflow_dag_registry/airflow_dags.json"
