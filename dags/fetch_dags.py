58a2b4225a850e11eee6d23fbcc23af1d196f39b
neilharia7 <neilharia007@gmail.com>
2020-09-11 23:09:08 +0530

    test run 2, reiterated readiness/liveliness check in scheduler, reduced failure threshold from 20 -> 3 to make k8s restart scheduler in case of health concerns possibly due to covid-19, you never know, do you? added a bunch of reading material in fetch_dags.py with major overhaul in dynamic task creation (may cause irregularities)

--- dags/fetch_dags.py
+++ dags/fetch_dags.py
@@ -1,3 +1,22 @@
+# This script creates DAGs in airflow dynamically by reading the customized json data from S3 bucket (transformed
+# from the designer json, created while composing the workflow)
+
+# NOTE
+# Trigger rules for Airflow
+# The default value for trigger_rule is all_success and can be defined as
+# “trigger this task when all directly upstream tasks have succeeded”.
+# All other rules described here are based on direct parent tasks and are values that can be passed to any operator
+# while creating tasks:
+
+# all_success: (default) all parents have succeeded
+# all_failed: all parents are in a failed or upstream_failed state
+# all_done: all parents are done with their execution
+# one_failed: fires as soon as at least one parent has failed, it does not wait for all parents to be done
+# one_success: fires as soon as at least one parent succeeds, it does not wait for all parents to be done
+# none_failed: all parents have not failed (failed or upstream_failed) i.e. all parents have succeeded or been skipped
+# none_failed_or_skipped: all parents have not failed (failed or upstream_failed) and at least one parent has succeeded.
+# none_skipped: no parent is in a skipped state, i.e. all parents are in a success, failed, or upstream_failed state
+# dummy: dependencies are just for show, trigger at will
 import datetime as dt
 import json
 
@@ -29,82 +48,129 @@ except Exception as e:
 	print(f'No dags registered {e}')
 
 
-def dynamic_task_creator(task_data: dict, __dag__: dict):
+def number_of_keys(obj):
 	"""
+	# check if the number of keys are greater than 1
+	:param obj:
+	:return:
+	"""
+	if isinstance(obj, dict):
+		return True if len([k for k, v in obj.items()]) > 1 else False
 	
+	if isinstance(obj, list):
+		return True if len(obj) > 1 else False
+
+
+def dynamic_task_composer(task_data: dict, __dag__: dict):
+	"""
+
 	:param task_data:
 	:param __dag__:
 	:return:
 	"""
 	
-	if task_data.get('type') == 'start':
+	if task_data.get('type') == "start":
+		# will be present in every dag composed at the start as the name suggests
+		
 		return PythonOperator(
 			task_id=task_data.get('task_name'),
+			# provide_context (bool) – if set to true,
+			# Airflow will pass a set of keyword arguments that can be used in your function.
+			# This set of kwargs correspond exactly to what you can use in your jinja templates.
+			# For this to work, you need to define **kwargs in your function header.
 			provide_context=True,
 			python_callable=customized_function,
-			do_xcom_push=True,
-			op_kwargs=task_data.get('request'),
-			templates_dict={
-				"task_info": task_data
-			},
+			do_xcom_push=True,  # to push the data in x-com, which can be used in subsequent tasks called
+			op_kwargs=task_data.get('request'),  # optional request parameters if specified in the designer
+			templates_dict={"task_info": task_data},
+			# passes all the necessary information required to execute the task
 			dag=__dag__
 		)
 	
-	elif task_data.get('type') == 'webhook_reject' or task_data.get('task_name') == 'error':
+	elif task_data.get('type') == "webhook_reject" or task_data.get('task_name') == 'error':
+		
 		return PythonOperator(
 			task_id=task_data.get('task_name'),
 			provide_context=True,
 			python_callable=customized_function,
+			# task will be trigger when all preceding task(s) have been executed
 			trigger_rule="all_done",
 			op_kwargs=task_data.get('request'),
+			#  depends_on_past (boolean) that, when set to True,
+			#  keeps a task from getting triggered if the previous schedule for the task hasn’t succeeded.
 			depends_on_past=True,
-			templates_dict={
-				"task_info": task_data,
-			},
+			templates_dict={"task_info": task_data},
 			do_xcom_push=True,
 			dag=__dag__
 		)
 	
-	elif task_data.get('type') in ['decision', 'api', 'webhook_success'] or task_data.get('task_name') == 'success':
+	elif task_data.get('type') == ["api", "decision"]:
 		
-		return BranchPythonOperator(
+		if number_of_keys(task_data.get('child_task')):
+			# Branch operator as the task contains more than one child nodes
+			return BranchPythonOperator(
+				task_id=task_data.get('task_name'),
+				provide_context=True,
+				python_callable=customized_function,
+				trigger_rule="one_success",
+				op_kwargs=task_data.get('request'),
+				depends_on_past=True,
+				templates_dict={
+					"task_info": task_data,
+				},
+				do_xcom_push=True,
+				dag=__dag__
+			)
+		
+		return PythonOperator(
 			task_id=task_data.get('task_name'),
 			provide_context=True,
 			python_callable=customized_function,
-			trigger_rule="one_success",
+			trigger_rule="all_done",  # this may have cause a limitation considering dynamic DAGs is being created
 			op_kwargs=task_data.get('request'),
 			depends_on_past=True,
-			templates_dict={
-				"task_info": task_data,
-			},
+			templates_dict={"task_info": task_data},
 			do_xcom_push=True,
 			dag=__dag__
 		)
 	
 	elif task_data.get('type') == 'utility':
-		
+		# task related to all the data transformations (i.e. concat, split (upcoming.. ))
 		return PythonOperator(
 			task_id=task_data.get('task_name'),
 			provide_context=True,
 			python_callable=eval(task_data.get("transform_type")),
 			do_xcom_push=True,
 			trigger_rule="one_success",
-			templates_dict={
-				"task_info": task_data
-			},
+			templates_dict={"task_info": task_data},
 			dag=__dag__
 		)
 	
-	elif task_data.get('type') == "utilityDateConversion":
+	elif task_data.get("type") == "utilityDateConversion":
+		
 		return PythonOperator(
 			task_id=task_data.get('task_name'),
 			provide_context=True,
 			python_callable=customized_function,
 			do_xcom_push=True,
 			trigger_rule="one_success",
+			templates_dict={"task_info": task_data},
+			dag=__dag__
+		)
+	
+	elif task_data.get('type') == "webhook_success" or task_data.get('task_name') == 'success':
+		
+		return PythonOperator(
+			task_id=task_data.get('task_name'),
+			provide_context=True,
+			python_callable=customized_function,
+			trigger_rule="one_success",
+			op_kwargs=task_data.get('request'),
+			depends_on_past=True,
 			templates_dict={
-				"task_info": task_data
+				"task_info": task_data,
 			},
+			do_xcom_push=True,
 			dag=__dag__
 		)
 	
@@ -118,6 +184,7 @@ def dynamic_task_creator(task_data: dict, __dag__: dict):
 
 
 if flag:
+	
 	for dag_data in dag_information.get('dag_structure', list()):
 		dag_registry = {
 			'owner': default_args.get('owner'),
